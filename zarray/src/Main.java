22f9fee888bd49b65b364b333b87d12fd982db0b
sleittt <a892737400@gmail.com>
2025-02-12 19:30:46 +0300

    Update Main.java

--- zarray/src/Main.java
+++ zarray/src/Main.java
@@ -5,13 +5,16 @@ public class Main {
         int[] B={2,3,4,5,6,7};
         int[] C={1,2,3,4};
         int[] D={2,3,4,5,6};
-        int[] AB=Sum(A,B);
-        int[] CD=Sum(C,D);
+        int[] X={1,2,3,4};
+        int[] AB=Sum(A,B,X);
+        int[] CD=Sum(C,D,X);
         int[] Z=Division(AB,CD);
         System.out.println(Arrays.toString(Z));
+        System.out.println(Arrays.toString(Search(Z,X)));
         }
 
-    public static int[] Sum(int [] A, int [] B) {
+    public static int[] Sum(int [] A, int [] B,int[] X) {
+        A=Multiplication(A,X);
         int[] AB=new int[A.length*B.length];
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < B.length; j++) {
@@ -21,6 +24,15 @@ public class Main {
         return AB;
     }
 
+    public static int[] Multiplication(int[] A, int[] B) {
+        int[] AB=new int[A.length*B.length];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                AB[(i*B.length)+j]=A[i]*B[j];
+            }
+        }
+        return AB;
+    }
     public static int[] Division(int[] A, int [] B) {
         int[] AB=new int[A.length*B.length];
         for (int i = 0; i < A.length; i++) {
@@ -29,5 +41,17 @@ public class Main {
             }
         }
         return AB;
+    }
+
+    public static int[] Search(int[] A, int[]B) {
+        int[] AB = new int[9600];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 9600; j++) {
+                if (A[j] == B[i]) {
+                    AB[j]=A[j];
+                }
+            }
+        }
+        return Arrays.stream(AB).filter(x -> x != 0).toArray();
     }
     }
