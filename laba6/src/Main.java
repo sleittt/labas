3513e2dad6ef213b66a197000c97c35b1ea948c8
sleittt <a892737400@gmail.com>
2025-02-23 20:53:29 +0300

    Лабораторная работа 6

--- laba6/src/Main.java
+++ laba6/src/Main.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 class Reader{
     String name;
-    static int id=1;
+    int id;
     String facultet;
     String DoB;
     String p_number;
-    Reader(String name, String facultet, String DoB, String p_number){
+    Reader(String name,int id, String facultet, String DoB, String p_number){
         this.name = name;
+        this.id=id;
         this.facultet= facultet;
         this.DoB=DoB;
         this.p_number=p_number;
@@ -29,11 +30,11 @@ class Reader{
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-            Reader P = new Reader("Петров В.В", "Информатика", "10.03.2006", "892798765432");
-            Reader A = new Reader("Антонов А.А", "Архитектура", "16.06.2008", "892731165432");
-            Reader B = new Reader("Баранов Б.Б", "Педагогика", "20.05.2007", "892791945432");
-            Reader C = new Reader("Сидоров С.С", "Пожарная безопасность", "19.01.2007", "892798645932");
-            Reader D = new Reader("Маркарян А.Г", "Кассир", "30.02.1798", "8912345698765");
+            Reader P = new Reader("Петров В.В",12345, "Информатика", "10.03.2006", "892798765432");
+            Reader A = new Reader("Антонов А.А",12346, "Архитектура", "16.06.2008", "892731165432");
+            Reader B = new Reader("Баранов Б.Б",12347, "Педагогика", "20.05.2007", "892791945432");
+            Reader C = new Reader("Сидоров С.С",12348, "Пожарная безопасность", "19.01.2007", "892798645932");
+            Reader D = new Reader("Маркарян А.Г",12349, "Кассир", "30.02.1798", "8912345698765");
             Reader[] readers= new Reader[]{P,A,B,C,D};
         System.out.println("Сколько книг взял читатель? ");
         int kol=in.nextInt();
