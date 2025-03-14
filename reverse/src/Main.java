03b820a969dc2ab0b7af8d87d0ee031da36ee2f5
sleittt <a892737400@gmail.com>
2025-03-14 10:14:25 +0300

    Доп Обратные числа

---
+++ reverse/src/Main.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Введите А");
+        double a=in.nextInt();
+        System.out.println("Введите mod");
+        double mod=in.nextInt();
+        for (int i = 2; i <= a; i++) {
+            if (a%i==0 && mod%i==0){
+                System.out.println("Числа не взаимнопростые");
+            }
+        }
+        for (int i = 1; i < mod; i++) {
+            if ((a * i)%mod == 1) {
+                System.out.println("Искомое число= " + i);
+            }
+        }
+    }
+}
