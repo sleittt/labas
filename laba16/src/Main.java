e37761766ae69ef1a3f5964f139f7486af0ba59f
sleittt <a892737400@gmail.com>
2025-04-25 09:57:22 +0300

    Лабораторная работа 16

--- laba16/src/Main.java
+++ laba16/src/Main.java
@@ -7,22 +7,24 @@ public class Main {
             String line = br.readLine();
             while (line != null) {
                 //System.out.println(line);
-                line = br.readLine();
-                if (line.contains("//") && (line.charAt(0) != '/') ){
+                if ((line.contains("//") && (line.charAt(0) != '/')) && (!line.contains("/\""))){
                     fw.write(line.substring(0,line.indexOf("//")) + "\n");
                     System.out.println(line.substring(0,line.indexOf("//")) + "\n");
-                } else if ((line.contains("//") && (line.charAt(0) == '/') || line.contains("/*"))) {
+                }
+                else if ((line.contains("//") && (line.charAt(0) == '/'))) {
+                }
+                else if(line.contains("/*") && (!line.contains("/*\""))){
+                    fw.write(line.substring(0,line.indexOf("/*"))+"\n");
+                    System.out.println(line.substring(0,line.indexOf("/*")));
+                    while(!line.contains("*/")){
+                        line=br.readLine();
+                    }
                 }
                 else {
                     fw.write(line + "\n");
                     System.out.println(line);
                 }
-
-                if(line.contains("/*")){
-                    while(!line.contains("*/")){
-                        line=br.readLine();
-                    }
-                }
+                line = br.readLine();
             }
         }
         catch (IOException e) {
